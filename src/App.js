2b78b8f3b599d047f1954cb9bec9539126178d13
Sridhar Reddy <sridhar.reddy@myob.com>
2019-11-02 14:00:59 +1300

    Add Prettier for consistent code style
    Local changes: vs code: Install prettier extension, enable formatOnSave setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function greet(language, name) {
+  switch (language) {
+    case 'es':
+      return 'Hola ' + name + '!'
+    case 'gb':
+      return 'Halo ' + name
+    case 'en':
+    default:
+      return 'Hello ' + name
+  }
 }
 
-export default App;
+function App({ lang, name }) {
+  return <div className="App">{greet(lang, name)}</div>
+}
+
+export default App
